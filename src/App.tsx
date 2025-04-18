f09487081c2fb6b45f3f26f223f4de84fdb3c84a
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-04-18 23:09:57 +0300

    base pages created

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
   ThemeProvider,
   createTheme,
 } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes";
 
 const darkTheme = createTheme({
   palette: {
@@ -16,10 +18,10 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container>
-        <h1>Dark Mode</h1>
+        <RouterProvider router={router} />
       </Container>
     </ThemeProvider>
   );
 };
 
-export default App
+export default App;
