ceb02fb1570e4ec4f9ba393fb8b84bd7b60866cc
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-04-24 17:21:15 +0300

    update

--- src/components/auth/Guard.tsx
+++ src/components/auth/Guard.tsx
@@ -1,6 +1,7 @@
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import excludedRoutes from "../../constants/excluded-routes";
 import { useGetMe } from "../../hooks/useGetMe";
+import { authenticatedVar } from "../../constants/authenticated";
 
 interface GuardProps {
   children: JSX.Element;
@@ -9,6 +10,12 @@ interface GuardProps {
 const Guard = ({ children }: GuardProps) => {
   const { data: user } = useGetMe();
 
+  // Check if the user is authenticated and set the authenticated variable accordingly
+  useEffect(() => {
+    if (user) {
+      authenticatedVar(true);
+    }
+  }, [user]);
   return (
     <>
       {excludedRoutes.includes(window.location.pathname)
