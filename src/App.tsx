e249acae66f871bfb34fe04ff1de3922759d72b0
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2025-04-28 18:20:15 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import client from "./constants/apollo-client";
 import Guard from "./components/auth/Guard";
 import Header from "./components/header/Header";
 import Snackbar from "./components/snackbar/Snackbar";
+import ChatList from "./components/chat-list/ChatList";
 import { usePath } from "./hooks/usePath";
 
 const darkTheme = createTheme({
@@ -23,15 +24,23 @@ const darkTheme = createTheme({
 const App = () => {
   const { path } = usePath();
 
+  const showChatList = path === "/" || path.includes("chats");
+
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Header />
         <Guard>
-          {path === "/" ? (
+          {showChatList ? (
             <Grid container>
-             
+              <Grid size={{  md: 3 }}>
+              <ChatList />
+              </Grid>
+              <Grid size={{  md: 9 }}>
+              <Routes />
+              </Grid>
+              
             </Grid>
           ) : (
             <Routes />
@@ -45,7 +54,7 @@ const App = () => {
 
 const Routes = () => {
   return (
-    <Container>
+    <Container sx={{ height: "100%" }}>
       <RouterProvider router={router} />
     </Container>
   );
